44eb8e3622a18dccd02849b457717ac49dfe0a2b
ayshasiddiqua513 <22it026@skcet.ac.in>
2025-01-06 20:53:11 +0530

    form completed

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const path = require('path');
+const fs = require('fs');
 const app = express();
 const port =80;
 
 //EXPRESS SPECIFIC
 app.use('/static', express.static('static'));
+app.use(express.urlencoded())
 
 //PUG SPECIFIC
 app.set('view engine','pug');
@@ -14,6 +16,19 @@ app.set('views', path.join(__dirname, 'views'));
 app.get('/',(req,res)=>{
     const con = "Get Gym membership of 30$ for 3 months!!!";
     const params = {'title':'pub is used in express for html','content':con}
+    res.status(200).render('index.pug',params)
+})
+
+app.post('/',(req,res)=>{
+    names = req.body.name
+    age = req.body.age
+    gender = req.body.gender
+    address = req.body.address
+    more = req.body.more
+    let outputToWrite = `the name of the member is ${names}, ${age} years old, ${gender}, residing at ${address}. More about him/her: ${more}`
+
+    fs.writeFileSync('output.txt',outputToWrite)
+    const params = {'message':'Your form has been submitted successfully'}
     res.status(200).render('index.pug',params)
 })
 
